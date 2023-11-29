3a442450f48815ff5cb8c1226ef545f874867cc2
Sergio Almela <sergioalmela@gmail.com>
2023-11-29 22:27:45 +0100

    Add mocks

---
+++ tests/mock/offers.mock.ts
@@ -0,0 +1,71 @@
+export const offersMock = [
+  {
+    affiliateUrl: 'https://www.testurl.com',
+    edition: '1',
+    id: 134977773,
+    isActive: true,
+    merchant: '557',
+    platform: 'PS5',
+    price: {
+      eur: {
+        bestCoupon: {
+          code: 'AKS10',
+          discountStrategy: '%',
+          discountValue: '10.00',
+          isCashback: false,
+        },
+        currency: 'eur',
+        price: 24.74,
+        priceWithoutCoupon: 27.49,
+      },
+    },
+    region: '9',
+    stock: 'InStock',
+  },
+  {
+    affiliateUrl: 'https://www.testurl.com',
+    edition: '1',
+    id: 135023156,
+    isActive: true,
+    merchant: '408',
+    platform: 'PS5',
+    price: {
+      eur: {
+        bestCoupon: {
+          code: '14%: AKS14',
+          discountStrategy: '%',
+          discountValue: '14.00',
+          isCashback: false,
+        },
+        currency: 'eur',
+        price: 25.09,
+        priceWithoutCoupon: 29.17,
+      },
+    },
+    region: '9',
+    stock: 'InStock',
+  },
+  {
+    affiliateUrl: 'https://www.testurl.com',
+    edition: '1',
+    id: 134470740,
+    isActive: true,
+    merchant: '47',
+    platform: 'steam',
+    price: {
+      eur: {
+        bestCoupon: {
+          code: '12%: AKS12',
+          discountStrategy: '%',
+          discountValue: '12.00',
+          isCashback: false,
+        },
+        currency: 'eur',
+        price: 25.71,
+        priceWithoutCoupon: 29.22,
+      },
+    },
+    region: '9',
+    stock: 'InStock',
+  },
+]
