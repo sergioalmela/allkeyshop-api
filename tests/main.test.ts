3bcdc23cf5b569ca5a8927c463b7e09457df8547
Sergio Almela <sergioalmela@gmail.com>
2023-12-05 22:45:47 +0100

    Make main tests more atomic

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,5 +1,4 @@
 import { AllkeyshopService } from '../src/allkeyshop'
-import { defaultOptions } from '../config/constants'
 
 describe('Allkeyshop', () => {
   it('AKS Main class is a class', () => {
@@ -12,33 +11,5 @@ describe('Allkeyshop', () => {
 
   it('AKS Main class has a search method', () => {
     expect(typeof AllkeyshopService.prototype.search).toBe('function')
-  })
-
-  it('AKS Main class has a search method that returns a promise', () => {
-    const allkeyshopService = new AllkeyshopService({
-      currency: defaultOptions.currency,
-      platform: defaultOptions.platform,
-      store: defaultOptions.store,
-    })
-
-    expect(allkeyshopService.search('Dark Souls')).toBeInstanceOf(Promise)
-  })
-
-  it('AKS Main class with default initialization has a search method that returns a promise', () => {
-    const allkeyshopService = new AllkeyshopService()
-
-    expect(allkeyshopService.search('Dark Souls')).toBeInstanceOf(Promise)
-  })
-
-  it('AKS Main class has a search method that returns a promise that resolves to an object', async () => {
-    const allkeyshopService = new AllkeyshopService({
-      currency: defaultOptions.currency,
-      platform: defaultOptions.platform,
-      store: defaultOptions.store,
-    })
-
-    await expect(
-      allkeyshopService.search('Dark Souls')
-    ).resolves.toBeInstanceOf(Object)
   })
 })
